9ff0b4e655013bd942584d75dd546573db972a40
ColumbiaCoding <gabriel.regus@gmail>
2023-09-12 22:28:22 -0400

    adding video chat and app code together

---
+++ Solved/js/index.js
@@ -0,0 +1,15 @@
+var session = OT.initSession(apiKey, sessionId);
+
+// create publisher
+var publisher = OT.initPublisher();
+session.connect(token, function(err) {
+   // publish publisher
+   
+});
+
+session.publish(publisher);
+
+session.on('streamCreated', function(event) {
+    session.subscribe(event.stream);
+ });
+
